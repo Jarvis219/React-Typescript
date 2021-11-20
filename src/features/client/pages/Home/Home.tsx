534c19c64290ebdb06385ef3cee2470219c761fe
Tran Anh Quang <quangtaph12673@fpt.edu.vn>
2021-11-20 18:35:01 +0700

    update css module build successfully

--- src/features/client/pages/Home/Home.tsx
+++ src/features/client/pages/Home/Home.tsx
@@ -1,12 +1,10 @@
 import { Header } from "features/client/components/Header/Header";
 import { Banner } from "features/client/components/Banner/Banner";
 import Store from "features/client/components/Store/Store";
-import { Loading } from "utils/loading/Loading";
 
 export default function Home() {
   return (
     <div>
-      <Loading />
       <Header />
       <Banner />
       <Store />
