76c2494bc6dffd11b46aae8f5a3ac0830bcf6874
Tran Anh Quang <quangtaph12673@fpt.edu.vn>
2021-12-04 16:15:05 +0700

    update code

--- servers/controllers/auth.js
+++ servers/controllers/auth.js
@@ -1,6 +1,5 @@
 import jwt from 'jsonwebtoken';
 import _ from 'lodash';
-
 import { v4 as uuidv4 } from 'uuid';
 const User = require('../model/userModel');
 const nodemailer = require('nodemailer');
